5e55dc94695d599ab94093a0e681190c86df2a13
Phiona Namugga <phionanamugga15@gmail.com>
2020-12-27 17:03:17 +0300

    integrates app with firebase real time database[#176288243]

---
+++ UI/JS/index.js
@@ -0,0 +1,11 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "noteapp-a371b.firebaseapp.com",
+    databaseURL: "https://noteapp-a371b.firebaseio.com",
+    projectId: "noteapp-a371b",
+    storageBucket: "noteapp-a371b.appspot.com",
+    messagingSenderId: "1003714616257",
+    appId: "1:1003714616257:web:fd44a9b7d8ffd43109ad40",
+    measurementId: "G-MEASUREMENT_ID",
+  };
